6deefa3dee575a43d7ba82e5a64b4bede8aab5ac
Gabriel Branco <gabriel01022227@gmail.com>
2024-12-13 21:12:35 +0000

    menu funcionando

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
 import streamlit as st
+from streamlit_option_menu import option_menu
 
-def pagina_1():
-    st.write('teste1')
+selected = option_menu(None, ["Configurações", "Orçamento", "Lançamentos", 'Dashboard', 'Relatórios'], 
+    icons=['sliders', 'cash', "list-ul", 'graph-up', 'file-earmark-bar-graph'], 
+    default_index=0, orientation="horizontal",
+    styles={
+        "container": {"max-width": "680px!important"}
+    })
 
-def pagina_2():
-    st.write('teste5')
 
-
-page_names_to_funcs = {
-    "Movimentações": pagina_1,
-    "Dashboard": pagina_2
-}
-
-demo_name = st.sidebar.selectbox("", page_names_to_funcs.keys())
-page_names_to_funcs[demo_name]()
+if selected == "Configurações":
+    st.title("Configurações")
+    st.write("Aqui você pode ajustar suas preferências.")
+    
+elif selected == "Orçamento":
+    st.title("Orçamento")
+    st.write("Aqui você pode gerenciar seu orçamento.")
+    
+elif selected == "Lançamentos":
+    st.title("Lançamentos")
+    st.write("Aqui você pode registrar seus lançamentos financeiros.")
+    
+elif selected == "Dashboard":
+    st.title("Dashboard")
+    st.write("Aqui você pode ver as visualizações dos seus dados financeiros.")
+    
+elif selected == "Relatórios":
+    st.title("Relatórios")
+    st.write("Aqui você pode acessar os relatórios financeiros.")
